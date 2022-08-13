6eaf99ba762ccf0092da72740970e9036502fd52
sndwvv <songparkhk@gmail.com>
2022-08-14 01:58:04 +0900

    Initial Commit

---
+++ SwiftChartsAudioVisualizer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SwiftChartsAudioVisualizer
+//
+//  Created by Songyee Park on 2022/08/14.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftChartsAudioVisualizer/SwiftChartsAudioVisualizerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftChartsAudioVisualizerApp.swift
+//  SwiftChartsAudioVisualizer
+//
+//  Created by Songyee Park on 2022/08/14.
+//
+
+import SwiftUI
+
+@main
+struct SwiftChartsAudioVisualizerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
